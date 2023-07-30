4f58db67b52f1e6ca93722a9fff41c5e94175459
Aashu623 <aashukushwah53678@gmail.com>
2023-07-30 06:13:09 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import './App.css';
+import Home from "./components/Home/Home";
+import Auth from "./components/Auth/Auth";
+import { auth, getUserFromDatabase } from "./firebase";
+import Spinner from './components/Spinner/Spinner';
+import Account from './components/Account/Account';
+function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const [userDetails, setUserDetails] = useState({});
+  const [isDataLoaded, setIsDataLoaded] = useState(false);
+
+  const fetchUserDetails = async (uid) => {
+    const userDetails = await getUserFromDatabase(uid);
+    setUserDetails(userDetails);
+    setIsDataLoaded(true);
+  }
+  useEffect(() => {
+    const listener = auth.onAuthStateChanged((user) => {
+      if (!user) {
+        setIsDataLoaded(true);
+        setIsAuthenticated(false);
+        return;
+      }
+
+      setIsAuthenticated(true);
+      fetchUserDetails(user.uid);
+    });
+
+    return () => listener();
+  }, []);
+  return (
+    <div>
+      <Router>
+        {isDataLoaded ? (
+
+
+          <Routes>
+            {!isAuthenticated && (
+              <>
+                <Route path='/signup' element={<Auth signup />} />
+                <Route path='/login' element={<Auth />} />
+              </>
+            )
+            }
+            <Route path='/account' element={<h1>{<Account userDetails={userDetails} auth={isAuthenticated} />}</h1>} />
+            <Route path='/' element={<Home auth={isAuthenticated} />} />
+            <Route path='/*' element={<Navigate to='/' />} />
+          </Routes>
+        ) : (
+          <Spinner />
+        )}
+      </Router>
+    </div>
+  );
+}
+
+export default App;
