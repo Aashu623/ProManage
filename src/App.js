53a0248946f122e47c8c51f4efbb52779232aab1
Aashu623 <aashukushwah53678@gmail.com>
2024-06-11 23:47:35 +0530

    Router Update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
 import Home from "./components/Home/Home";
 import Auth from "./components/Auth/Auth";
 import { auth, getUserFromDatabase } from "./firebase";
-import Spinner from './components/Spinner/Spinner';
 import Account from './components/Account/Account';
 function App() {
+
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   const [userDetails, setUserDetails] = useState({});
-  const [isDataLoaded, setIsDataLoaded] = useState(false);
 
   const fetchUserDetails = async (uid) => {
     const userDetails = await getUserFromDatabase(uid);
     setUserDetails(userDetails);
-    setIsDataLoaded(true);
   }
   useEffect(() => {
     const listener = auth.onAuthStateChanged((user) => {
       if (!user) {
-        setIsDataLoaded(true);
         setIsAuthenticated(false);
         return;
       }
@@ -31,27 +28,31 @@ function App() {
 
     return () => listener();
   }, []);
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home auth={isAuthenticated} />
+    },
+    {
+      path: '/*',
+      element: <Home auth={isAuthenticated} />
+    },
+    {
+      path: '/account',
+      element: <Account userDetails={userDetails} auth={isAuthenticated} />
+    },
+    {
+      path: '/signup',
+      element: <Auth signup />
+    },
+    {
+      path: '/login',
+      element: <Auth />
+    },
+  ])
   return (
-    <div>
-      <Router>
-        {isDataLoaded ? (
-          <Routes>
-            {!isAuthenticated && (
-              <>
-                <Route path='/signup' element={<Auth signup />} />
-                <Route path='/login' element={<Auth />} />
-              </>
-            )
-            }
-            <Route path='/account' element={<Account userDetails={userDetails} auth={isAuthenticated} />} />
-            <Route path='/' element={<Home auth={isAuthenticated} />} />
-            <Route path='/*' element={<Navigate to='/' />} />
-          </Routes>
-        ) : (
-          <Spinner />
-        )}
-      </Router>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
